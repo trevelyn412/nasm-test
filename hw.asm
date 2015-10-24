34a204300d231ae45e6b72f119e083c7498ce2f4
root <trevelyn412>
2015-10-24 00:21:07 +0000

    Added the hw.asm file!

---
+++ hw.asm
@@ -0,0 +1,17 @@
+section .data ; initialized data:
+	msg: db "Hello World!",0xA
+	msgLen: equ $-msg
+section .text ; our logic sauce:
+	global _start
+	_start:
+		mov EAX, 4 ; syscall for sys_write()
+		mov EBX, 1 ; /dev/stdout
+		mov ECX, msg ; 
+		mov EDX, msgLen ; length of message in bytes
+	
+		int 0x80 ; call kernel
+
+		mov EAX, 1 ; syscall for exit()
+		mov EBX, 0 ; return 0
+		int 0x80 ; call the kernel and pass execution to her
+section .bss ; no uninitialized data in this application yet
